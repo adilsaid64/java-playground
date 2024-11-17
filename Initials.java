4fffcb9848c08004d0569f30cdaa96445923e4d6
adil <adilsaid64@gmail.com>
2024-11-17 14:04:49 +0000

    feat: initial commit

---
+++ Initials.java
@@ -0,0 +1,15 @@
+public class Initials {
+    public static void main(String[] args) {
+        printAS();
+    }
+
+    public static void printAS() {
+        System.out.println("    A       SSSSS");
+        System.out.println("   A A     S     S");
+        System.out.println("  A   A    S");
+        System.out.println(" AAAAAAA     SSSSS");
+        System.out.println(" A     A          S");
+        System.out.println("A       A   S     S");
+        System.out.println("A       A    SSSSS");
+    }
+}
